9883219d2a174d311078a4f9f53950496ab87d3f
dov1s <maojunwei1@huawei.com>
2025-05-14 19:38:06 +0800

    Add code 05/15/2015

---
+++ build/CMakeFiles/leptjson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson.dir/src/leptjson.cpp.o"
+  "CMakeFiles/leptjson.dir/src/leptjson.cpp.o.d"
+  "libleptjson.a"
+  "libleptjson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leptjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/leptjson_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson_test.dir/tests/test.cpp.o"
+  "CMakeFiles/leptjson_test.dir/tests/test.cpp.o.d"
+  "leptjson_test"
+  "leptjson_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leptjson_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
